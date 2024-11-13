74712157515ccf68d88216b9f80a024e8bcd6ed0
dhyan-aranha <dhyanaranha@Dhyans-MBP.home>
2024-11-13 18:29:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Morse-Lemma
---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "Morse-Lemma" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «MorseLemma» where
+  -- add any library configuration options here
